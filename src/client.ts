94233fc8583757a1a4d26fa763459d867a39e47b
rvelasquez <rvelasquez@celtech.com.gt>
2020-06-26 01:29:30 -0600

    Se han añadido archivo fuente de dato y clase para consumir servicio con axios

---
+++ src/client.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import * as devconf from "dotenv";
+import data from "./data.json";
+
+devconf.config();
+
+const postInfo = async () => {
+  try {
+    await axios.post(`${process.env.BASE_URL}`, {
+      headers: {},
+      data: { data },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getInfo = async () => {
+  try {
+    const res = await axios.get(`${process.env.BASE_URL}:${process.env.MAIL}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "x-password": process.env.ACCESS_KEY,
+      },
+    });
+    const data = res.data;
+    console.log("Resultado del Get", data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getInfo(), postInfo();
